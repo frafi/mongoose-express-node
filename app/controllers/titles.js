67e708f5c839c406182e5a7b6ef602b3fdfa9d50
Fawad Rafi <frafi@exammaster.com>
2013-10-27 21:55:26 -0400

    Fixed the index action

--- app/controllers/titles.js
+++ app/controllers/titles.js
@@ -24,39 +24,43 @@ exports.load = function(req, res, next, id){
 /**
  * List
  */
-
-exports.index = function(req, res){ 
-  var regExpSearch = /./
-  if (req.param('title') != 'undefined' && req.param('title') != '') {
-  	regExpSearch = new RegExp(req.param('title'), "i")
-  }
-  var searchTitle = (req.param('title') != 'undefined' && req.param('title') != '') ? '/'+req.param('title')+'/i' : ''
-  var page = (req.param('page') > 0 ? req.param('page') : 1) - 1
-  var perPage = 30
-  var options = {
-    perPage: perPage,
-    page: page,
-    criteria: {
+exports.index = function(req, res){
+  if (typeof(req.param('title')) !== "undefined") {
+ 	var regExpSearch = (req.param('title') != '') ? new RegExp(req.param('title'), "i") : /./
+ 	var searchTitle = (req.param('title') != 'undefined' && req.param('title') != '') ? '/'+req.param('title')+'/i' : ''
+    var page = (req.param('page') > 0 ? req.param('page') : 1) - 1
+    var perPage = 30
+    var options = {
+      perPage: perPage,
+      page: page,
+      criteria: {
     	TitleNameSortable: regExpSearch
+      }
     }
-  }
-  Title.list(options, function(err, titles) {
-    if (err) return res.render('500')
-    Title.count().exec(function (err, count) {
-      res.render('titles/index', {
-        title: 'Movie Titles',
-        titles: titles,
-        page: page + 1,
-        pages: Math.ceil(count / perPage)
+    Title.list(options, function(err, titles) {
+      if (err) return res.render('500')
+      Title.count().exec(function (err, count) {
+        res.render('titles/index', {
+          title: 'Movie Titles',
+          titles: titles,
+          page: page + 1,
+          pages: Math.ceil(count / perPage)
+        })
       })
+    })  
+  }
+  else
+  {
+  	res.render('titles/index', {
+      title: 'Movie Titles',
+      titles: []
     })
-  })  
+  }
 }
 
 /**
  * Show
  */
-
 exports.show = function(req, res){
   res.render('titles/show', {
     title: req.title
